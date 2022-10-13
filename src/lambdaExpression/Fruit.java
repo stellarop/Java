8d0411e5fbccbc8788035e4710e86874f9fd9431
leeyeonjae990415 <roson741@gmail.com>
2022-10-13 23:18:00 +0900

    OperatorMinByMaxByExample class 사용을 위한 Fruit class

---
+++ src/lambdaExpression/Fruit.java
@@ -0,0 +1,20 @@
+package lambdaExpression;
+
+// OperatorMinByMaxByExample class 를 이용하기 위한 class
+public class Fruit {
+    private String name;
+    private int price;
+
+    public Fruit(String name, int price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getPrice(){
+        return price;
+    }
+}
