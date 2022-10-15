6d9a8db2ab38299005d99780486ba2083245c37e
leeyeonjae990415 <roson741@gmail.com>
2022-10-16 01:01:42 +0900

    람다식 확인 문제를 구현하기 위한 class

---
+++ src/lambdaExpression/LambdaExample2St.java
@@ -0,0 +1,26 @@
+package lambdaExpression;
+
+// 람다식2를 참조하는 클래스
+public class LambdaExample2St {
+    private String name;
+    private int es;
+    private int ms;
+
+    public LambdaExample2St(String name, int es, int ms){
+        this.name = name;
+        this.es = es;
+        this.ms = ms;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getEs() {
+        return es;
+    }
+
+    public int getMs() {
+        return ms;
+    }
+}
