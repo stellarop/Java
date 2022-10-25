8bf71ccec4bbfd8dbd0463f7c4159880e1fe9a6a
leeyeonjae990415 <roson741@gmail.com>
2022-10-26 02:25:09 +0900

    Comparable 을 구현하지 않은 class

---
+++ src/collection/Fruit.java
@@ -0,0 +1,14 @@
+package collection;
+
+// Comparable 을 구현하지 않은 class
+public class Fruit {
+    public String  name;
+    public int price;
+
+    public Fruit(String name, int price){
+        this.name = name;
+        this.price = price;
+    }
+
+
+}
