3e12586b688985b96a8469c4179374e285a64b7c
leeyeonjae990415 <roson741@gmail.com>
2022-10-30 23:43:17 +0900

    LambdaExpressionExample 클래스를 사용하기 위한 Student 클래스

---
+++ src/stream/Student.java
@@ -0,0 +1,19 @@
+package stream;
+
+// LambdaExpressionExample 클래스를 사용하기 위한 Student 클래스
+public class Student {
+    private String name;
+    private int score;
+
+    public Student(String name, int score){
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getScore(){
+        return score;
+    }
+}
